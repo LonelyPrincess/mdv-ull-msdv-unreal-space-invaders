cf6f8f6a83f524ced841bfa2948d289add13f977
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-09-23 12:39:01 +0100

    feat: create gameframework c++ classes

---
+++ Source/SpaceInvaders/Public/SIGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEINVADERS_API ASIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
