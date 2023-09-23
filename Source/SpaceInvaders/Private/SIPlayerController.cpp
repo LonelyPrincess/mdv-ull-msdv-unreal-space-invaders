8a2f050cb0baef48c7cc3e6c2155e680f2abcd86
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-09-23 12:55:17 +0100

    feat: initialize main camera from player controller

--- Source/SpaceInvaders/Private/SIPlayerController.cpp
+++ Source/SpaceInvaders/Private/SIPlayerController.cpp
@@ -2,4 +2,21 @@
 
 
 #include "SIPlayerController.h"
+#include "Kismet/GameplayStatics.h"
 
+// Constructor where we assign initial size for cameras array
+ASIPlayerController::ASIPlayerController()
+	: m_mainCameraTag("MainCamera")			// This assigns default value to main camera tag prop
+{
+	m_cameras.Reserve(5);
+}
+
+// Begin play event is triggered when controller is ready to use
+void ASIPlayerController::BeginPlay() {
+	// Fetch all cameras available on scene and store them in array
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), m_mainCameraTag, m_cameras);
+
+	// Set the first camera tagged as "MainCamera" as the view target
+	if (m_cameras.Num() > 0)
+		this->SetViewTarget(m_cameras[0]);
+}
--- Source/SpaceInvaders/Public/SIPlayerController.h
+++ Source/SpaceInvaders/Public/SIPlayerController.h
@@ -14,4 +14,16 @@ class SPACEINVADERS_API ASIPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+
+	ASIPlayerController();
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY()
+	TArray<class AActor*> m_cameras;		// Array to store available cameras on scene
+	FName m_mainCameraTag;					// Variable to store tag which will be used to fetch cameras from the code
+
 };
