3a23b86b83472919996f8414babb564da758b301
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-10-01 18:59:58 +0100

    feat: create movement component for invaders

---
+++ Source/SpaceInvaders/Private/InvaderMovementComponent.cpp
@@ -0,0 +1,256 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InvaderMovementComponent.h"
+
+// Constructor to initialize class parameters
+UInvaderMovementComponent::UInvaderMovementComponent()
+	: state{ InvaderMovementType::STOP }
+	, horizontalVelocity{ 1000.0f }
+	, verticalVelocity{ 1000.0f }
+	, descendingStep{ 100.0f }
+	, freeJumpRadius{ 300.0f }
+	, freeJumpVelocity{ 1000.0f }
+	, deltaAlphaInterpolation{ 1.0f / 30.0f }
+	, numberOfTargetPoints{ 5 }
+	, previousState{ InvaderMovementType::STOP }
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+// Method called as soon as actor becomes active in scene
+void UInvaderMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Apply random angle so the invader is looking on a random direction at the start
+	finalAngle = FMath::RandRange(-30.0f, 30.0f);
+
+}
+
+// Generate a sequence of geometric transformation to perform a circular trajectory
+void UInvaderMovementComponent::GenerateTargetPoints() {
+
+	// Obtain actor to which the component is linked to, so we can fetch its location
+	AActor* Parent = GetOwner();
+	FTransform initialTransform;
+	FVector initialLocation;
+	FVector initialScale;
+
+	FQuat initialQuaternion;
+	FVector forward;
+
+	if (!Parent) {
+		numberOfTargetPoints = 0;
+		return;
+	}
+
+	initialTransform = Parent->GetActorTransform();
+	initialLocation = initialTransform.GetLocation();
+	initialScale = initialTransform.GetScale3D();
+	initialQuaternion = initialTransform.GetRotation();
+	forward = Parent->GetActorForwardVector();
+
+	// The first stage movement is a circle
+	// Calculate center of the circle from actor location
+	float radio = freeJumpRadius;
+	FVector center = initialLocation;
+
+	center.X += radio;
+
+	if (numberOfTargetPoints > 0) {
+		float theta = 0.0f; // Theta is the angle that will be applied on each waypoint
+		float deltaTheta = 2 * PI / numberOfTargetPoints;
+
+		// GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Blue, FString::Printf(TEXT("X is %g Y is %g"), initialLocation.X, initialLocation.Y));
+
+		FVector newLocation = initialLocation;
+		FRotator rotation;
+		FTransform newTransform = initialTransform;
+		for (int32 i = 0; i < numberOfTargetPoints; i++) {
+
+			float pc = FMath::Cos(theta);
+			float ps = FMath::Sin(theta);
+
+			newLocation.X = center.X - radio * FMath::Cos(theta);
+			newLocation.Y = center.Y + radio * FMath::Sin(theta);
+			newTransform.SetLocation(newLocation);
+
+			// Change the rotation of the actor to follow the tangent of the circle
+			if (i != (numberOfTargetPoints - 1)) {
+				// FRotator requires angles in degrees!
+				// Rotation is in Yaw. The following angle makes the invader to follow the tangent
+				rotation = FRotator(0.0f, -(theta * 180.0f / PI) - 90, 0.0f);
+				FQuat newQuaternion = rotation.Quaternion() * initialQuaternion;
+				newTransform.SetRotation(newQuaternion);
+			}
+			else // Last point set the invader with the initial transformation
+				newTransform.SetRotation(initialQuaternion); // Last transformation 
+			targetPoints.Add(newTransform);
+			theta += deltaTheta;
+		}
+
+	}
+
+}
+
+// The purpose of having target points is getting the transformations to apply in the trajectory through interpolation
+// The following function performs the interpolation
+FTransform UInvaderMovementComponent::InterpolateWithTargetPoints(FTransform origin, float fraction) {
+
+	FVector originLocation = origin.GetLocation();
+	FQuat originRotation = origin.GetRotation();
+	FVector targetLocation;
+	FQuat targetRotation;
+
+	if (currentTargetPoint >= 0 && currentTargetPoint < numberOfTargetPoints) {
+		targetLocation = targetPoints[currentTargetPoint].GetLocation();
+		targetRotation = targetPoints[currentTargetPoint].GetRotation();
+	}
+	else
+		return origin;
+
+	// Location interporlation for vectors (uses linear interpolation)
+	FVector newLocation = UKismetMathLibrary::VLerp(originLocation, targetLocation, fraction);
+
+	// Spherical interpolation for quaterions
+	FQuat newRotation = FQuat::Slerp(originRotation, targetRotation, fraction);
+
+	FTransform newTransform = origin;
+	newTransform.SetLocation(newLocation);
+	newTransform.SetRotation(newRotation);
+	return newTransform;
+
+}
+
+// Method called on every render
+void UInvaderMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AActor* Parent = GetOwner(); // Parent is the actor who owns this component.
+
+	if (!Parent)
+		return;
+
+	float deltaHorizontal = horizontalVelocity * DeltaTime; // Increment in horizontal and vertical dimensions given DeltaTime and parameterized velocities
+	float deltaVertical = verticalVelocity * DeltaTime;
+
+	float deltaX = 0.0f; // These deltas determine the change of the actor position
+	float deltaY = 0.0f;
+
+	// deltaX and deltaY are calculated differently for each movement type
+	// The movement type is in the public variable state
+	// previousState is updated
+	switch (state) {
+
+		// If movement type is stop, we don't apply any changes to rotation
+		case InvaderMovementType::STOP:
+			deltaX = 0.0f;
+			deltaY = 0.0f;
+			previousState = InvaderMovementType::STOP;
+			break;
+
+		// Turn right
+		case InvaderMovementType::RIGHT:
+
+			deltaX = 0.0f;
+			deltaY = deltaHorizontal;
+
+			previousState = InvaderMovementType::RIGHT;
+			break;
+
+		// Turn left
+		case InvaderMovementType::LEFT:
+
+			deltaX = 0.0f;
+			deltaY = -deltaHorizontal;
+			previousState = InvaderMovementType::LEFT;
+			break;
+
+		// Down movement: this is an automatic movement that has to finish automatically
+		// It is based on an internal variable, descendinfProgress, that is updated.
+		case InvaderMovementType::DOWN:
+
+			// If invader was not descending yet, set progress counter to initialize descending movement
+			if (previousState != InvaderMovementType::DOWN) {
+				descendingProgress = 0.0f; // This means that the down phase is starting
+			}
+
+			// If descending progress exceeds the distance we configured in step param, we must stop invader from going down anymore
+			if (descendingProgress > descendingStep) {
+				deltaVertical = 0.0f; // This means that the down phase stops
+			}
+
+			deltaX = -deltaVertical;
+			deltaY = 0.0f;
+			descendingProgress += deltaVertical;
+			previousState = InvaderMovementType::DOWN;
+			break;
+
+		// Free jump movement: this is an automatic complex movement.
+		case InvaderMovementType::FREEJUMP:
+
+			deltaX = 0.0f; // This movement is not based on deltaX, deltaY, but in general transformations
+			deltaY = 0.0f;
+
+			// If invader has just got in free jump state, generate waypoints to choose how it'll move
+			if (previousState != InvaderMovementType::FREEJUMP) { // First time we enter in FREEJUMP
+				GenerateTargetPoints();
+				currentTargetPoint = 0;
+				if (numberOfTargetPoints > 0) {
+					originTransform = Parent->GetActorTransform(); // First originTransform for interpolation is actor transform
+					alphaInterpolation = 0.0f;
+				}
+
+				previousState = InvaderMovementType::FREEJUMP;
+			}
+
+			// Now the movement is programatically defined.
+			// There are two stages:
+			// First stage: an automatic movement defined by a sequence of target transforms
+			// currentTargetPoint is the index of the current transform
+			if (currentTargetPoint < numberOfTargetPoints) {
+				// New transform calculated by interpolation between current and currentTargetPoint.
+				FTransform newtransform = InterpolateWithTargetPoints(originTransform, alphaInterpolation);
+				// The actor receive the new transform					
+				Parent->SetActorTransform(newtransform);
+
+				alphaInterpolation += deltaAlphaInterpolation;
+				if (alphaInterpolation > 1.0f) { // target has been reached with interpolation
+					++currentTargetPoint;
+					alphaInterpolation = 0.0f; // To start a new segment of linear interpolation
+
+					if (currentTargetPoint < numberOfTargetPoints) // new originTransform is previous target
+						originTransform = this->targetPoints[currentTargetPoint - 1];
+					// If this was the last target we add a random rotation that it will define the direction of the movement in the second stage of the free jump
+					else {
+						Parent->AddActorLocalRotation(FRotator(0.0f, finalAngle, 0.0f));
+					}
+				}
+			}
+
+			// Second stage: the actor is simply moved in the forward direction after having gone through all waypoints
+			else {
+				FVector parentLocation = Parent->GetActorLocation();
+				FVector forward = Parent->GetActorForwardVector();
+				FVector right = Parent->GetActorRightVector();
+				parentLocation += freeJumpVelocity * DeltaTime * forward;
+
+				Parent->SetActorLocation(parentLocation);
+			}
+	}
+
+	// Apply calculated deltaX deltaY for those movements based on them (right, left)
+	if (Parent && state != InvaderMovementType::FREEJUMP) {
+
+		FVector parentLocation = Parent->GetActorLocation();
+		parentLocation.X += deltaX;
+		parentLocation.Y += deltaY;
+		Parent->SetActorLocation(parentLocation);
+
+	}
+}
---
+++ Source/SpaceInvaders/Public/InvaderMovementComponent.h
@@ -0,0 +1,107 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/MovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "InvaderMovementComponent.generated.h"
+
+// Enum with all supported movement types
+//	(it's recommended to map all enum status to an integer of 8 bits, like this; otherwise they may not work in blueprints)
+//	(naming convention specifies that all enum types shall start with "E", like "EInvaderMovementType")
+UENUM(BlueprintType)
+enum class InvaderMovementType : uint8 {
+
+	STOP = 0 UMETA(DisplayName = "Stopped"),
+	RIGHT = 1 UMETA(DisplayName = "Right"),
+	LEFT = 2 UMETA(DisplayName = "Left"),
+	DOWN = 3 UMETA(DisplayName = "Down"),
+	FREEJUMP = 4 UMETA(DisplayName = "Free Jump")
+
+};
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEINVADERS_API UInvaderMovementComponent : public UMovementComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+	// Sets default values for this component's properties
+	UInvaderMovementComponent();
+
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Invader Movement")
+	float horizontalVelocity;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Invader Movement")
+	float verticalVelocity;
+
+	// Property to indicate current state of the object, which movement type is being applied
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Invader Movement")
+	InvaderMovementType state;
+
+
+	// Down movement parameters:
+	// Movement to go down will be done on fixed steps based on the value of this property
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Invader Movement")
+	float descendingStep; // Length of the descending step
+
+
+	// Free jump parameters:
+	// List of points that will create a circuit to determine how the invader will move (waypoints)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Invader Movement")
+	TArray<FTransform> targetPoints; // The movement will be composed by interpolation using a sequence of reference poses stored in this array.
+
+	// Number of waypoints in the sequence
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Invader Movement")
+	int32 numberOfTargetPoints;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Invader Movement")
+	float freeJumpRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Invader Movement")
+	float freeJumpVelocity; // Velocity in the first stage of the free jump
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Invader Movement")
+	float deltaAlphaInterpolation; // Number of updates between two target points
+
+public:
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// Generate waypoints for current invader based on their current location
+	UFUNCTION(BlueprintCallable)
+	void GenerateTargetPoints();
+
+	UFUNCTION(BlueprintCallable)
+	FTransform InterpolateWithTargetPoints(FTransform transform, float covered);
+
+private:
+
+	// This stores the previous movement direction of the invader, so we can resume it later
+	//	(ex.: if user was moving right and then descends, this variable can be used to keep moving right once descend has finished)
+	InvaderMovementType previousState; // Store state in previous frame (to know when a state is beginning)
+
+	// Down movement state variables:
+	float descendingProgress = 0.0f; // Store progress in the Down state
+
+	// Free jump movement state variables:
+	FTransform originTransform;
+	float alphaInterpolation;
+	int32 currentTargetPoint = 0; // It stores the index of the first reference pose (the other is currentTargetPoint+1)
+
+	// Random angle which will determine direction in which the invader will attack
+	float finalAngle; // Orientation of the invader to start the final attack
+
+};
